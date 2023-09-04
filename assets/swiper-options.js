cfda2db49a680c51a22cf14fb95d52f162824cbe
petrov-codedot <petrov.codedot@gmail.com>
2023-09-04 12:24:07 +0300

    Fix visual bugs

--- assets/swiper-options.js
+++ assets/swiper-options.js
@@ -3,8 +3,7 @@ const swiperOptions = [
         name: "cart-product-slider",
         options: {
             loop: false,
-            direction: "horizontal",
-            mousewheel: true
+            direction: "horizontal"
         }
     },
     {
